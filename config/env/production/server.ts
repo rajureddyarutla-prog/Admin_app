55f551e80d11f10c4827d3de21a1757d5331d241
RajuReddyArutla <rajureddy@hypermiles.com>
2026-03-10 10:22:10 +0530

    env updateds

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,7 +1,7 @@
 import type { Core } from '@strapi/strapi';
 
 export default ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
-    url: env('RENDER_EXTERNAL_URL'),
+    url: env('URL', env('RENDER_EXTERNAL_URL')),
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     app: {
