8835765ac81c71f812f747e09335493c7b8fa9ba
RajuReddyArutla <rajureddy@hypermiles.com>
2026-03-10 10:28:26 +0530

    fix: resolve typescript database config error

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -5,6 +5,11 @@ export default ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database 
         client: 'postgres',
         connection: {
             connectionString: env('DATABASE_URL'),
+            host: env('DATABASE_HOST', 'localhost'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'strapi'),
+            user: env('DATABASE_USERNAME', 'strapi'),
+            password: env('DATABASE_PASSWORD', 'strapi'),
             ssl: env.bool('DATABASE_SSL', true) && {
                 rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
             },
